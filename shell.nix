2c0e4f93030ffb87df69e0dd67f06e1311e64c1b
David Chan <davidlechan@gmail.com>
2026-07-06 18:10:56 -0700

    local build support in nix :)

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    (pkgs.texlive.combine {
+      inherit (pkgs.texlive)
+        scheme-medium
+        titlesec
+        enumitem
+        fontawesome5
+        eso-pic
+        bookmark
+        lastpage
+        changepage
+        paracol
+        needspace
+        charter
+        latexmk;
+    })
+  ];
+}
